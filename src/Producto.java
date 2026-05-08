bf74543095784fe010f60f7f489acafa91024c30
Nicolás Calderón <calderondpc@gmail.com>
2026-05-08 11:34:37 -0300

    Avance: Lista de productos y bucle funcionando.

---
+++ src/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+
+    public String nombre;
+    public double precio;
+
+
+    public Producto(String nombreRecibido, double precioRecibido) {
+        this.nombre = nombreRecibido;
+        this.precio = precioRecibido;
+    }
+}
